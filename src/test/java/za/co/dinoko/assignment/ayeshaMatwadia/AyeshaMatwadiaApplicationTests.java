19ded99966e81652d605765dbbac4a1ba6a4f75c
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-19 20:58:11 +0200

    added the project structure for this file

---
+++ src/test/java/za/co/dinoko/assignment/ayeshaMatwadia/AyeshaMatwadiaApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.dinoko.assignment.ayeshaMatwadia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyeshaMatwadiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
